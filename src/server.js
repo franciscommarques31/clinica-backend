2a03bc7379170fdd1bc1eb10613e680901d8cd8a
Francisco Marques <franciscomarques31@gmail.com>
2026-04-16 12:03:09 +0100

    fix: usar porta do render

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
 });
 
 // PORTA
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Servidor a correr na porta ${PORT}`);
