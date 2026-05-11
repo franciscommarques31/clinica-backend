d30503ac3de796ba13cef2554130dfbe01eb7887
Francisco Marques <franciscomarques31@gmail.com>
2026-05-11 22:35:05 +0100

    Nova versão

--- src/routes/appointmentRequestRoutes.js
+++ src/routes/appointmentRequestRoutes.js
@@ -9,4 +9,13 @@ router.get("/", async (req, res) => {
   res.json(await Model.find());
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    await Model.findByIdAndDelete(req.params.id)
+    res.json({ success: true })
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao apagar pedido" })
+  }
+})
+
 module.exports = router;
