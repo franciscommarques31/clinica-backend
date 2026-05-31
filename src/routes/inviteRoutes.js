9e9a7b936d7586e8c27ceabd61950233983ce239
Francisco Marques <franciscomarques31@gmail.com>
2026-05-31 20:17:11 +0100

    Atualização do projeto

--- src/routes/inviteRoutes.js
+++ src/routes/inviteRoutes.js
@@ -4,7 +4,9 @@ const nodemailer = require("nodemailer");
 const Invite = require("../models/Invite");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
